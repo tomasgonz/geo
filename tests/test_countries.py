243e1aa6ccb46c2ab2eaefe45791d5dd2be79ef5
Tomas <tomasgonz@gmail.com>
2022-10-23 19:00:32 -0400

    Changes added to tests

--- tests/test_countries.py
+++ tests/test_countries.py
@@ -3,3 +3,7 @@ from paises import *
 def test_countries():
     c = countries.Countries()
     c.load_wb()
+    print ("test for countries passes")
+
+if __name__ == "__main__":
+    test_countries()
