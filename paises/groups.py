9c4019e47d71915682e56f4108c6f02ead7171a0
Tomas <tomasgonz@gmail.com>
2022-10-26 20:35:49 -0400

    Added support for new collection of groups as in countries

--- paises/groups.py
+++ paises/groups.py
@@ -1,20 +1,20 @@
-import os
+import json
 import importlib.resources
 
-def get_groups():
-    groups = []
-    for i in importlib.resources.path(__package__, "groups").iterdir():
-        groups.append(i.name.split(".")[0])
-    return groups
+class groups(list):
+	
+	def __init__(self):
+		if len(self) == 0:
+			pass
 
-def get_countries(group):
-    countries = []
-    path = group + ".json"
-    file_path = importlib.resources.path(__package__, "countries.json")
+	def load_groups(self):
+		for i in importlib.resources.path(__package__, "groups").iterdir():
+			print("Loading " + i.name)
+			self.append(json.load(i.open(encoding='utf-8')))
 
-
-        
-    
-    
-    
-
+		return groups
+	
+	def get_countries(self, group):
+		countries = []
+		path = group + ".json"
+		file_path = importlib.resources.path(__package__, "countries.json")
--- tests/test_countries.py
+++ tests/test_countries.py
@@ -1,11 +1,9 @@
-import sys
 from paises import Countries
 from paises.groups import *
-
-import importlib.resources
 
 def test_countries():
     c = Countries()
     c.load_wb()
+
 def test_groups():
-    print(get_groups())
+    print(load_groups())
